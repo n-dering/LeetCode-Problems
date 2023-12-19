0922df2b8b8faf3386e2903f29b68597c2ef6b7f
Abey <norbertdering@gmail.com>
2023-12-19 21:00:01 +0100

    remove print statements

--- 88 Merge Sorted Array/SortedArray.py
+++ 88 Merge Sorted Array/SortedArray.py
@@ -1,4 +1,4 @@
-#PROBLEM LINK https://leetcode.com/problems/merge-sorted-array/
+# PROBLEM LINK https://leetcode.com/problems/merge-sorted-array/
 from typing import List
 
 
@@ -7,7 +7,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2
+        nums1[3:] = nums2
         nums1.sort()
 
         zeros_count = nums1.count(0)
